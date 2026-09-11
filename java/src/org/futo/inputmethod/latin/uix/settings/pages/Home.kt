bfb9b96d49b6e55a4854ffa66d24fcc7296311e5
david419kr <david419kr@gmail.com>
2026-09-12 00:30:08 +0900

    미결제 팝업 제거

--- java/src/org/futo/inputmethod/latin/uix/settings/pages/Home.kt
+++ java/src/org/futo/inputmethod/latin/uix/settings/pages/Home.kt
@@ -105,17 +105,6 @@ val HomeScreenLite = UserSettingsMenu(
             icon = R.drawable.themes
         ),
 
-        //if(!isPaid) {
-        userSettingNavigationItem(
-            title = R.string.payment_screen_short_title,
-            style = NavigationItemStyle.HomePrimary,
-            navigateTo = "payment",
-            icon = R.drawable.dollar_sign,
-        ).copy(visibilityCheck = {
-            useDataStoreValue(IS_ALREADY_PAID) == false
-        }, appearInSearchIfVisibilityCheckFailed = false),
-        //}
-
         userSettingNavigationItem(
             title = R.string.help_menu_title,
             style = NavigationItemStyle.HomeSecondary,
@@ -154,7 +143,6 @@ fun HomeScreen(navController: NavHostController = rememberNavController()) {
     val context = LocalContext.current
     val scrollState = rememberScrollState()
     val isDeveloper = useDataStoreValue(IS_DEVELOPER)
-    val isPaid = useDataStoreValue(IS_ALREADY_PAID)
 
     Column {
         Column(
@@ -181,22 +169,11 @@ fun HomeScreen(navController: NavHostController = rememberNavController()) {
             }
 
             ConditionalMigrateUpdateNotice()
-            ConditionalUnpaidNoticeWithNav(navController)
 
             HomeScreenLite.render(showTitle = false)
 
 
             Spacer(modifier = Modifier.height(16.dp))
-
-            if(isPaid || LocalInspectionMode.current) {
-                Text(
-                    stringResource(R.string.payment_paid_version_indicator),
-                    style = Typography.SmallMl,
-                    modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Center
-                )
-                Spacer(modifier = Modifier.height(12.dp))
-            }
 
             Text(
                 "v${BuildConfig.VERSION_NAME}",
